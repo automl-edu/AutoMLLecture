34e14b8bbd01b2e36f6f1232518fb5f26aa2fb96
Bernd Bischl <bernd_bischl@gmx.net>
2020-06-09 00:16:44 +0200

    fix t02 1000 things

--- w08_hpo_multicrit/code/sms_emoa_hvcontrib.R
+++ w08_hpo_multicrit/code/sms_emoa_hvcontrib.R
@@ -8,11 +8,9 @@ dd$c2_max = dd$c2
 n = nrow(dd)
 
 oo = order(dd$c1, decreasing = TRUE)
-print(oo)
 for (j in 2:n)
   dd[oo[j], "c1_max"] = dd[oo[j-1], "c1"]
 oo = order(dd$c2, decreasing = TRUE)
-print(oo)
 for (j in 2:n)
   dd[oo[j], "c2_max"] = dd[oo[j-1], "c2"]
 
@@ -25,10 +23,11 @@ pl = ggplot(dd, aes(x = c1, y = c2))
 pl = pl + geom_point(size = 3)
 pl = pl + geom_rect(aes(xmin = c1, xmax = c1_max, ymin = c2, ymax = c2_max), alpha = 0.2)
 # pl = pl + geom_point(x = 9, y = 9, size = 3)
-# pl = pl + xlim(0,9) + ylim(0, 9)
+pl = pl + xlim(0,10) + ylim(0, 10)
 # pl = pl + geom_text(aes(x = 5, y = 7, label = expression(lambda)))
-pl = pl + geom_text(data = dd[-n,], aes(x = c1+0.7, y = c2+0.2, label = paste("HVC =",hvc)))
-pl = pl +  annotate("text", x = dd$c1[j_worst]-0.2, y = dd$c2[j_worst]-0.2, size = 7, parse = TRUE, label = as.character(expression(tilde(lambda))))
-print(pl)
+pl = pl + geom_text(data = dd[-n,], aes(x = c1+1, y = c2+0.5, label = paste("HVC =",hvc)), size = 3)
+pl = pl +  annotate("text", x = dd$c1[j_worst]-0.5, y = dd$c2[j_worst]-0.5, size = 5, parse = TRUE, label = as.character(expression(tilde(lambda))))
+
+ggsave(pl, file = "images/hv_contrib.png", width = 3, height = 3)
 
 
